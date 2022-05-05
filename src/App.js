f5802f81901b42e083a5ca9322f4e2565e2391ad
JoshuahBoardman <joshuahboardman@gmail.com>
2022-05-04 20:30:30 -0500

    Created and implemented the Footer component.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // components
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
     <Header />
+    <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light text-center text-lg-start">
+      <div className="text-center p-3">
+        © 2022 Copyright:
+        <a className="text-dark text-decoration-none" href="https://mdbootstrap.com/">
+          {" "}
+          JoshuahBoardman.com
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
